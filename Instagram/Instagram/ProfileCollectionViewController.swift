c4a5737960f6e4a968762fec0c088d48be237ea0
Kelvin Lee <leeweisern@gmail.com>
2016-11-17 14:33:13 +0800

    First Push

--- Instagram/Instagram/ProfileCollectionViewController.swift
+++ Instagram/Instagram/ProfileCollectionViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+
 
 private let reuseIdentifier = "profileCell"
+private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+private let itemsPerRow: CGFloat = 3
 
 class ProfileCollectionViewController: UICollectionViewController{
-
+    var users = User()
+    var arrayOfPicturesUrl = [String]()
+    var arrayOfFollowers = [String]()
+    var arrayOfFollowings = [String]()
+    var userUID = "Admin1"
+    
+    var ref: FIRDatabaseReference!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = FIRDatabase.database().reference()
         
         //add tap gesture to edit profiel
         
@@ -27,47 +40,192 @@ class ProfileCollectionViewController: UICollectionViewController{
        
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        fetchUser()
+        fetchFollowers()
+        fetchFollowings()
+        
+    }
+    
+    //fetch user info
+    func fetchUser() {
+        
+        
+        ref.child("User").child(userUID).observeSingleEvent(of: .value, with: {(snapshot) in
+            
+            
+            
+            if let dictionary = snapshot.value as? [String:AnyObject] {
+                self.users.desc = (dictionary["desc"] as! String?)!
+                self.users.name = (dictionary["name"] as! String?)!
+                self.users.picture = (dictionary["picture"] as! String?)!
+                self.users.posted = (dictionary["posted"] as! [String : String]?)!
+            
+                let imageurlArray = self.users.posted
+                for imageurl in imageurlArray {
+                    
+                    self.arrayOfPicturesUrl.append(imageurl.value)
+                    print(imageurl.value)
+                    print((String)(self.arrayOfPicturesUrl.count))
+             
+                    
+                    self.collectionView?.reloadData()
+                    
+                }
+                
+            }
+            
+        })
+    }
 
+    func fetchFollowers() {
+        ref.child("Follower").child(userUID).observe(.value, with: {(snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                
+                print(dictionary.keys)
+                
+                for key in dictionary.keys {
+                    self.arrayOfFollowers.append(key)
+                }
+                
+                self.collectionView?.reloadData()
+            
+            
+        }
+            
+        })
+      
+    }
 
-
+    func fetchFollowings() {
+        ref.child("Following").child(userUID).observe(.value, with: {(snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                
+                print(dictionary.keys)
+               
+                for key in dictionary.keys {
+                    self.arrayOfFollowings.append(key)
+                }
+                
+                self.collectionView?.reloadData()
+                
+                
+            }
+            
+        })
+        
+ 
+    }
     // MARK: UICollectionViewDataSource
 
-//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return 1
+       
+        return self.users.posted.count + 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == 0 {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProfileCollectionViewCell
     
         // insert number of post followers etc here
-        cell.numberOfPosts.text = "23"
-        cell.numberOfFollowing.text = "343"
-        cell.numberOfFollowers.text = "543"
+        cell.numberOfPosts.text = (String)(self.users.posted.count)
+        cell.numberOfFollowing.text = (String)(self.arrayOfFollowings.count)
+        cell.numberOfFollowers.text = (String)(self.arrayOfFollowers.count)
+        cell.profileFullName.text = self.users.name
+        cell.profileDescription.text = self.users.desc
+            
         
         
         //fonts layout
         cell.numberOfPosts.font = UIFont.boldSystemFont(ofSize: 16)
         cell.numberOfFollowers.font = UIFont.boldSystemFont(ofSize: 16)
         cell.numberOfFollowing.font = UIFont.boldSystemFont(ofSize: 16)
+            
+        //view layout
+//        cell.viewContainingButtons.layer.borderWidth = 0.5
+//        let grayColor = UIColor(red: 128/255, green: 128/255 , blue: 128/255 , alpha: 1)
+//        cell.viewContainingButtons.layer.borderColor = grayColor.cgColor
+            
         
         
         //circular profile picture
-        cell.profilePicture.image = #imageLiteral(resourceName: "pichu")
-        cell.profilePicture.layer.cornerRadius = cell.profilePicture.frame.height / 2
-        cell.profilePicture.clipsToBounds = true
-        cell.profilePicture.layer.borderWidth = 1.0
-        cell.profilePicture.layer.masksToBounds = false
-        
+        if self.users.picture != "" {
+            cell.profilePicture.loadImageUsingCacheWithUrlString(self.users.picture)
+            cell.profilePicture.layer.cornerRadius = cell.profilePicture.frame.height / 2
+            cell.profilePicture.clipsToBounds = true
+            cell.profilePicture.layer.borderWidth = 1.0
+            cell.profilePicture.layer.masksToBounds = true
+            cell.profilePicture.contentMode = .scaleAspectFill
+        }
 
-        
+       
         return cell
+        } else {
+            
+             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pictureCell", for: indexPath) as! GridLayoutCollectionViewCell
+            
+            var url = ""
+             if self.arrayOfPicturesUrl.count > 0 {
+            url = self.arrayOfPicturesUrl[indexPath.row - 1]
+            print("********** \(url)")
+            //cell.profileImages.loadImageUsingCacheWithUrlString(url)
+            let imgdata = NSData(contentsOf: NSURL(string: url)! as URL)
+            let img = UIImage(data: imgdata as! Data)
+            cell.profileImages.image = img
+            
+            
+            }
+            return cell
+        }
+        
+        
+    }
+    
+}
+
+
+
+    //layout
+    extension ProfileCollectionViewController : UICollectionViewDelegateFlowLayout {
+        //1
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            sizeForItemAt indexPath: IndexPath) -> CGSize {
+            
+            
+            if indexPath.row != 0 {
+                //2
+                let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+                let availableWidth = view.frame.width - paddingSpace
+                let widthPerItem = availableWidth / itemsPerRow
+            
+                return CGSize(width: widthPerItem, height: widthPerItem)
+            } else{
+                
+                
+                //make it dynamic
+                return CGSize(width: 375, height: 260)
+
+            }
+        }
+        
+        //3
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            insetForSectionAt section: Int) -> UIEdgeInsets {
+            return sectionInsets
+        }
+        
+        // 4
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return sectionInsets.left
+        }
     }
     
     // MARK: UICollectionViewDelegate
@@ -82,4 +240,4 @@ class ProfileCollectionViewController: UICollectionViewController{
     */
 
    
-}
+
---
+++ Instagram/Instagram/User.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by NEXTAcademy on 11/16/16.
+//  Copyright © 2016 Wherevership. All rights reserved.
+//
+
+import Foundation
+
+
+public class User{
+    
+    var name: String = ""
+    var desc: String = ""
+    var picture: String = ""
+    var posted : [String : String] = [:]
+}

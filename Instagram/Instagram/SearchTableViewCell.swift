3947be3b3c386d7301d10d707db25029bc8c657e
micropop1989 <lcg1989@hotmail.com>
2016-11-18 12:54:21 +0800

    update search function

--- Instagram/Instagram/SearchTableViewCell.swift
+++ Instagram/Instagram/SearchTableViewCell.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class SearchTableViewCell: UITableViewCell {
 
+   
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
+   }
